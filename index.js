d81536f267cdf64ac1c53af9f6d379613e3b0149
MD Yasin <mdyasin48902@gmail.com>
2026-04-21 23:15:16 +0600

    mongodb added and user data save  api added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const app = express(); 
+const app = express();
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,7 +11,69 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// test route
+app.get("/", (req, res) => {
+  res.send("DevFlow-server running...");
+});
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGO_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // database collections
+    const database = client.db("DevFlow");
+    usersCollection = database.collection("users");
+
+    // users data save
+    app.post("/users", async (req, res) => {
+      try {
+        const { name, email, role } = req.body;
+
+        const user = {
+          name,
+          email,
+          role: role || "developer", // default role
+          createdAt: new Date(),
+        };
+
+        const result = await usersCollection.insertOne(user);
+
+        res.send({
+          success: true,
+          message: "User registered successfully",
+          data: result,
+        });
+      } catch (error) {
+        res.status(500).send({
+          success: false,
+          message: error.message,
+        });
+      }
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+run();
 
 // server start
 app.listen(PORT, () => {
