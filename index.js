c9210a81111e6e3a6c4623ea6088e1d028d76ef9
MD Yasin <mdyasin48902@gmail.com>
2026-05-27 12:50:38 +0600

    update cors origin

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://devflow-32d85.web.app",
     credentials: true,
   }),
 );
